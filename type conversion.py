2eb6e6c598ad649f92cb691bdefbd31444f9405f
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-05-12 23:50:11 +0530

    Create type conversion.py

---
+++ type conversion.py
@@ -0,0 +1,26 @@
+# type conversion
+a=12      # integer automatic convert to float becasee  float take higher value
+b=4.33
+sum= a+b
+print(sum)  # so esks answer will be 12.0 + 4.33 = 16.33
+
+a = "12"
+b=  4.33 
+sum(a+b) # this thing will not work becahse we can not convert string to float string to string we can add
+
+
+# we can do type casting string value to float 
+c= float("22")
+v=4.23
+sum=c+v
+print (sum)
+
+ # here we have convert into string value to int 
+n= int("6")
+v=4.2
+sum=n+v
+print(type(v))
+print(sum)
+
+
+
