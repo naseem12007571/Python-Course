d7c80a619a29245e5c1c5c61b0628db345486669
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-05-13 23:48:56 +0530

    Create sum of two number.py

---
+++ sum of two number.py
@@ -0,0 +1,21 @@
+first = int(input("First number  is "))
+second = int(input("second  number is "))
+print("sum of two number is ",first+second)
+print("multiply of two number is ",first*second)
+print("minus  number is ",first-second)
+print("devide of two number is ",first/second)
+print("module of two number is ",first%second)
+
+# average of two  number 
+# a=float(input("Enter the number "))
+# b=float(input("Enter the number "))
+# print("average is =", a+b/2)
+
+
+
+
+
+
+
+
+
