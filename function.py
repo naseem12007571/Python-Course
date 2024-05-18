6e2616926371e7c9c2defa1f5e79a2f206248b1e
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-05-18 23:35:30 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,17 @@
+# def means funciton defination
+def cal_sum(a,b):   # function parameter
+    return a + b
+sum=cal_sum(1,5)    # fuction call and argument 1 and 5
+print(sum)
+
+
+
+# questionnde
+def cal_avg(a,b,c):
+    sum= a+b+c
+    avg=sum/3
+    print(avg)
+    return avg
+cal_avg(5,6,7)
+
+    
